d6a05f27eb199656299c245cf5c4bcfa9b4cd21f
spa173 <131736103+spa173@users.noreply.github.com>
2026-05-17 09:01:37 -0500

    fix(config): actualizar preview.config.js con factory function en el wrapper de vue para prevenir reinicios de estado

--- preview.config.js
+++ preview.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from '@previewjs/config';
 import { createPinia } from 'pinia';
 import { createMemoryHistory, createRouter } from 'vue-router';
 
-const pinia = createPinia();
-const router = createRouter({
-  history: createMemoryHistory(),
-  routes: [],
-});
-
 export default defineConfig({
   wrapper: {
-    vue: {
-      plugins: [pinia, router]
-    }
-  }
+    vue: () => {
+      const pinia = createPinia();
+      const router = createRouter({
+        history: createMemoryHistory(),
+        routes: [],
+      });
+      return {
+        plugins: [pinia, router],
+      };
+    },
+  },
 });
