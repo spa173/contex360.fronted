ddbd9008b3ae16b527a1c67d2de182a5696fc313
spa173 <131736103+spa173@users.noreply.github.com>
2026-05-17 08:59:50 -0500

    fix(config): remover imports de css en preview.config.js para compatibilidad con esbuild en preview.js

--- preview.config.js
+++ preview.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from '@previewjs/config';
 import { createPinia } from 'pinia';
 import { createMemoryHistory, createRouter } from 'vue-router';
-import './src/style.css';
-import './src/assets/styles.css';
 
 const pinia = createPinia();
 const router = createRouter({
